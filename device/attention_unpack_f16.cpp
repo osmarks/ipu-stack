99051189a783c561d27de88b93d9a5fc91eab6c3
osmarks <me@osmarks.net>
2026-07-22 02:33:53 +0000

    Vectorize attention output unpacking

--- device/attention_unpack_f16.cpp
+++ device/attention_unpack_f16.cpp
@@ -18,12 +18,12 @@ public:
   bool compute(unsigned worker) {
     for (unsigned row = worker; row < rows; row += 6) {
       for (unsigned column = 0; column < ATTENTION_HEAD_DIMENSION;
-           column += 2) {
+           column += 4) {
         const unsigned outputColumn = columnStart + column;
         const unsigned destination = (outputColumn / 16) * rows * 16 +
                                      row * 16 + outputColumn % 16;
-        *reinterpret_cast<half2 *>(&output[destination]) =
-            *reinterpret_cast<const half2 *>(
+        *reinterpret_cast<half4 *>(&output[destination]) =
+            *reinterpret_cast<const half4 *>(
                 &source[row * ATTENTION_HEAD_DIMENSION + column]);
       }
     }
